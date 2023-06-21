54fc9fd137a009135753cbec931016b563f7fd40
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-21 15:20:25 +0530

    button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Alert from "./components/Alert";
-
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
       <Alert> Hello, world</Alert>
+      <Button color="primary" onClick={() => console.log("Clicked")}>
+        Submit
+      </Button>
     </div>
   );
 }
