53eb7ac7d8cc8675616c2dcd465691682142db13
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-21 12:38:16 +0530

    props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const cities = ["Mumbai", "Bangalore", "chennai"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={cities} heading="Cities" />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = [
-    "New Delhi",
-    "Mumbai",
-    "Bengaluru",
-    "Chennai",
-    "Pune",
-    "Hydarabad",
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
-    <ul className="list-group">
-      {items.map((item, index) => (
-        <li
-          key={item}
-          className={
-            selectedIndex === index
-              ? "list-group-item active"
-              : "list-group-item"
-          }
-          onClick={() => setSelectedIndex(index)}
-        >
-          {item}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
