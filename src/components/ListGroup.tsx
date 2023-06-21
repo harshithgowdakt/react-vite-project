fb470cd68a23c36ba1789be2e6a52176665e5be0
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-21 11:37:34 +0530

    dynamic list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,21 @@
 function ListGroup() {
-  return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
-  );
+const items = [
+  "New Delhi",
+  "Mumbai",
+  "Bengaluru",
+  "Chennai",
+  "Pune",
+  "Hydarabad",
+];
+return (
+  <ul className="list-group">
+    {items.map((item) => (
+      <li key={item} className="list-group-item">
+        {item}
+      </li>
+    ))}
+  </ul>
+);
 }
 
 export default ListGroup;
