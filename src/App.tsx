da8829abdf299f5a9bdee2241c5332dd4695c8e2
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-21 14:24:09 +0530

    add children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+
 
 function App() {
   return (
     <div>
-      <Alert text="Hello, world"></Alert>
+      <Alert> Hello, world</Alert>
     </div>
   );
 }
