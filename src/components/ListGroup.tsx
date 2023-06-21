3ce13615f3457ae748b50d77e6526f5f0f25ed41
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-21 12:09:31 +0530

    onclick\

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,26 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-const items = [
-  "New Delhi",
-  "Mumbai",
-  "Bengaluru",
-  "Chennai",
-  "Pune",
-  "Hydarabad",
-];
-return (
-  <ul className="list-group">
-    {items.map((item) => (
-      <li key={item} className="list-group-item">
-        {item}
-      </li>
-    ))}
-  </ul>
-);
+  const items = [
+    "New Delhi",
+    "Mumbai",
+    "Bengaluru",
+    "Chennai",
+    "Pune",
+    "Hydarabad",
+  ];
+
+  const handleClick = (event: MouseEvent) =>
+    console.log("Event: ", event.detail);
+  return (
+    <ul className="list-group">
+      {items.map((item, index) => (
+        <li key={item} className="list-group-item" onClick={handleClick}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ListGroup;
