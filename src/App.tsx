4ef0c80b039950991b48b4d9c1becb0966e1eb36
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-22 12:38:27 +0530

    update array

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import { useState } from "react";
 function App() {
   const [game, setGame] = useState({ id: 1, player: { name: "harshith" } });
 
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
+  });
+
   const handleClick = () => {
     setGame({ ...game, player: { ...game.player, name: "gowda" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "cheese"] });
   };
 }
 
