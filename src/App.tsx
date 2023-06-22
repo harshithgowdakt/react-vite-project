2d8374d715bf3d36e32b92aff55ff9a9823f63ff
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-22 12:46:10 +0530

    update state

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,23 @@ function App() {
     toppings: ["Mushroom"],
   });
 
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quatity: 1 },
+      { id: 2, title: "Product 2", quatity: 1 },
+    ],
+  });
+
   const handleClick = () => {
     setGame({ ...game, player: { ...game.player, name: "gowda" } });
     setPizza({ ...pizza, toppings: [...pizza.toppings, "cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quatity: item.quatity + 1 } : item
+      ),
+    });
   };
 }
 
