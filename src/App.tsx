22c635d6aa161b945e0111199e7f5861c97f682a
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-22 23:47:24 +0530

    update objects

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [drink, setDrink] = useState({
+    title: "KF",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      {drink.price}
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
