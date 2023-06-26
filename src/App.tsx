e5cf8d2c423744153e7221b85f0b69716de01485
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-26 12:34:11 +0530

    update users

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,22 @@ function App() {
       });
   };
 
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updateUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -72,12 +88,22 @@ function App() {
             className="list-group-item d-flex justify-content-between"
           >
             {u.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(u)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => {
+                  updateUser(u);
+                }}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(u)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
